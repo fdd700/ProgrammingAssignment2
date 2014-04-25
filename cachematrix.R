1eeb495c3cb1f5b169b23254b88fade56a0de0a4
fdd700 <fdd700@rdpa.org>
2014-04-25 19:36:07 +1000

    working functions according to the specs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCacheMatrix function creates the matrix
+# and getter and setter methods for it
+# and for the inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(m) m <<- m
+  getInv <- function() m
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+  
 }
 
-
-## Write a short comment describing this function
+# cacheSolve function checks if Inv matrix exists in cache
+# if it does, it returns it
+# if it can't be found, it is solved, cached and returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
